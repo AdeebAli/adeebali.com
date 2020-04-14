1951121622e800801c50aef70de476ab3de4cbd2
Adeeb Ali <22379775+AdeebAli@users.noreply.github.com>
2020-04-13 23:42:29 -0500

    Initial Commit

---
+++ .github/README.md
@@ -0,0 +1,4 @@
+# adeebali.com
+The code for my website.
+
+![Node.js CI](https://github.com/AdeebAli/adeebali.com/workflows/Node.js%20CI/badge.svg?branch=master)
---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+const Index = () => {
+	return (
+		<main>
+			<div>
+				Adeeb&apos;s website written in NextJS.
+			</div>
+		</main>
+	);
+};
+
+export default Index;
