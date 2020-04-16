fbb13c2a11cfc5334df0c9a5bed61ac014b05709
Adeeb Ali <22379775+AdeebAli@users.noreply.github.com>
2020-04-15 22:58:16 -0500

    adding head to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,12 @@
+import Head from 'next/head';
+
 const Index = () => {
 	return (
 		<main>
+			<Head>
+				<title>Adeeb Ali</title>
+			</Head>
+
 			<div>
 				Adeeb&apos;s website written in NextJS.
 			</div>
