f2f23e434e2af569c119427061e0ba6a03c10e9d
Adeeb Ali <22379775+AdeebAli@users.noreply.github.com>
2020-05-31 02:04:47 -0500

    remove console log

--- src/components/theme.js
+++ src/components/theme.js
@@ -5,7 +5,6 @@ breakpoints.sm = breakpoints[0];
 breakpoints.md = breakpoints[1];
 breakpoints.lg = breakpoints[2];
 breakpoints.xl = breakpoints[3];
-console.log(breakpoints.sm);
 const customTheme = {
 	...theme,
 	colors: {
