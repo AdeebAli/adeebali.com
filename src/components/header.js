f86a1688a4b14367b96d84c5608985b38f815801
Adeeb Ali <22379775+AdeebAli@users.noreply.github.com>
2024-05-19 02:36:04 -0500

    fix navbar closing and sizing issues on mobile

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,5 @@
-import Link from 'next/link';
-import {Heading, IconButton, useColorMode, Flex, Box, useOutsideClick} from '@chakra-ui/react';
+import NextLink from 'next/link';
+import {IconButton, useColorMode, Flex, Box, useOutsideClick, Stack, Text, Link} from '@chakra-ui/react';
 import {MoonIcon, SunIcon} from '@chakra-ui/icons';
 import {MdMenu, MdClose} from 'react-icons/md';
 import {useRef, useState} from 'react';
@@ -24,71 +24,62 @@ const Header = props => {
 
 	return (
 		<Flex
+			ref={ref}
 			as='nav'
-			padding={scrollPosition > 80 ? '2em' : '2.5em'}
 			transitionDuration='0.4s'
-			minHeight={scrollPosition > 80 ? '6em' : '7em'}
 			width='100%'
+			padding={scrollPosition > 80 ? 4 : 8}
 			bgGradient='linear(to-r, dark.shade, brand)'
 			color='white'
 			justify='space-between'
-			align={{base: 'center', md: 'center'}}
+			align='center'
 			position='fixed'
-			flexWrap='wrap'
-			shadow={show ? '2' : null
-			}
+			marginBottom={8}
+			wrap='wrap'
 			top={0}
-			zIndex={99}
+			zIndex={100}
 			{...props}
 		>
-			<Heading
-				as='h1'
-				ml='5'
-				size='md'
-				fontSize={20}
-			>
-				<Link passHref href='/'>
-					<Box fontWeight='bold'>
+
+			<Link size={'md'} fontSize={20} fontWeight='bold' as={NextLink} href='/'>
 						Adeeb Ali
-					</Box>
-				</Link>
-			</Heading>
+			</Link>
 
 			<Box
 				display={{base: 'block', md: 'none'}}
 				onClick={handleToggle}
 			>
-				<Box as={show ? MdClose : MdMenu}/>
+				{show ? <MdClose /> : <MdMenu />}
 			</Box>
-			<Flex
-				className='navItems'
-				alignItems='center'
-				textAlign='center'
-				fontSize={17}
-				display={{base: show ? 'block' : 'none', md: 'flex'}}
-				flexDir={{base: 'column', md: 'row'}}
-				width={{base: 'full', md: 'auto'}}
-				ml={{base: 6}}
+			<Box
+				display={{base: show ? 'block' : 'none', md: 'block'}}
+				flexBasis={{base: '100%', md: 'auto'}}
 			>
-				<Link passHref href='/'>
-					<Box mt={{base: 3, md: 0}} mr={6}>Home</Box>
-				</Link>
-				<Link passHref href='/resume'>
-					<Box mt={{base: 3, md: 0}} mr={6}>Resume</Box>
-				</Link>
-				<Box
-					mt={{base: 3, md: 0}}
-
+				<Stack
+					className='navItems'
+					align='center'
+					spacing={8}
+					justify={['center', 'space-between', 'flex-end', 'flex-end']}
+					direction={['column', 'row', 'row', 'row']}
+					fontSize={17}
+					pt={[4, 4, 0, 0]}
 				>
+					<Link as={NextLink} href='/'>
+						<Text display='block'>Home</Text>
+					</Link>
+					<Link as={NextLink} href='/resume'>
+						<Text display='block'>Resume</Text>
+					</Link>
 					<IconButton
 						colorScheme='purple'
-						mr={5}
+						display='block'
 						aria-label='Change color mode'
 						size='md' icon={colorMode === 'light' ? <MoonIcon/> : <SunIcon/>}
 						onClick={toggleColorMode}
 					/>
-				</Box>
-			</Flex>
+
+				</Stack>
+			</Box>
 		</Flex>
 	);
 };
